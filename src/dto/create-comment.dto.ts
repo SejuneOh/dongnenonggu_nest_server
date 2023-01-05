19923bcb1561716b8bfd403f4cb8ffc2343c8246
SejuneOh <etry0715@gmail.com>
2023-01-05 23:34:09 +0900

    [#2]feat: createCommentDto, createReplyDto @ApiProperty

--- src/dto/create-comment.dto.ts
+++ src/dto/create-comment.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsNumber, IsString } from 'class-validator';
 
 export class CreateCommentDto {
   @IsNumber()
+  @ApiProperty({ description: '댓글이 작성 된 게시글 번호' })
   boardNo: number;
 
   @IsString()
+  @ApiProperty({ description: '댓글 작성자 uuid ' })
   writerId: string;
 
   @IsString()
+  @ApiProperty({ description: '댓글 내용' })
   content: string;
 }
--- src/dto/create-reply.dto.ts
+++ src/dto/create-reply.dto.ts
@@ -1,8 +1,9 @@
 import { IsNumber } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
 import { CreateCommentDto } from './create-comment.dto';
 
 export class CreateReplyDto extends PartialType(CreateCommentDto) {
   @IsNumber()
+  @ApiProperty({ description: '답글이 작성된 댓글 고유 id' })
   readonly commentId: number;
 }
