5e3d30a60ee1860f8ca1e26acc892c0f62f82e48
SejuneOh <etry0715@gmail.com>
2023-01-05 20:58:40 +0900

    del: delete log

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,12 +1,9 @@
-import { UserController } from './user.controller';
 import { Injectable, NestMiddleware, Version, Body } from '@nestjs/common';
 
 @Injectable()
 export class UserMiddleware implements NestMiddleware {
   @Version('1')
   use(req: any, res: any, next: () => void) {
-    console.log('User Middle ware call');
-
     next();
   }
 }
